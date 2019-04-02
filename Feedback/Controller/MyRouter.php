ea251a43b4344a536c3c64bfa71fd9bfca4a53ea
System User <student4@ap72.sty>
2019-04-02 14:50:09 +0300

    task 4

---
+++ Feedback/Controller/MyRouter.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace Ravkovich\Feedback\Controller;
+
+
+class MyRouter implements \Magento\Framework\App\RouterInterface
+{
+    protected $actionFactory;
+    protected $_response;
+    protected $scopeConfig;
+
+    public function __construct(
+        \Magento\Framework\App\ActionFactory $actionFactory,
+        \Magento\Framework\App\ResponseInterface $response,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    )
+    {
+        $this->actionFactory = $actionFactory;
+        $this->_response = $response;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function match(\Magento\Framework\App\RequestInterface $request)
+    {
+        $identifier = trim($request->getPathInfo(), '/');
+        $my_url = $this->scopeConfig->getValue('feedback/general/frontend_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        if (strpos($identifier, $my_url) !== false) {
+            $request->setModuleName('myrouter')-> //module name
+            setControllerName('index')-> //controller name
+            setActionName('index'); //action name
+        } else {
+            return false;
+        }
+        return $this->actionFactory->create(
+            'Magento\Framework\App\Action\Forward',
+            ['request' => $request]
+        );
+    }
+}
