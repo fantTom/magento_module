688264d23b394738524fd2e5802174fd7e7d03c8
System User <student4@ap72copy.sty>
2019-04-04 18:21:30 +0300

    add xhr

--- Feedback/view/frontend/web/js/autocomplete.js
+++ Feedback/view/frontend/web/js/autocomplete.js
@@ -1,19 +1,25 @@
 require(['jquery'], function ($) {
     $(document).ready(function () {
+
+        var xhr = null;
         $('#sku').keyup(function () {
-            $.ajax({
+            if (xhr) {
+                xhr.abort();
+            }
+            xhr = $.ajax({
                 url: 'myrouter/prod/autocomplete',
                 data: $('#sku').serialize(),
-                beforeSend: function () {
-                    $("#sku-list").empty();
-                },
                 success: function (response) {
-
+                    $("#sku-list").empty();
                     for (var sku in response) {
                         $('#sku-list').append($('<option>').attr('value', sku).text(response[sku]));
                     }
+                },
+                complete: function () {
+                    xhr = null;
                 }
             });
+
         })
     });
 });
