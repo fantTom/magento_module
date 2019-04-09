eba67a00036f7c6a528fa7712772831c020bd81a
System User <student4@ap72copy.sty>
2019-04-09 10:12:03 +0300

    task 7, add task 6

--- Feedback/Plugin/AddProductToCart.php
+++ Feedback/Plugin/AddProductToCart.php
@@ -4,6 +4,9 @@
 namespace Ravkovich\Feedback\Plugin;
 
 
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+
 class AddProductToCart
 {
 
@@ -22,12 +25,24 @@ class AddProductToCart
     public function beforeExecute(\Magento\Checkout\Controller\Cart\Add $add)
     {
 
-        $product = $this->productRepository->get($add->getRequest()->getParam('sku'));
-        $add->getRequest()->setParams([
-            'product' => $product->getId(),
-            'qty' => $add->getRequest()->getParam('qty'),
-        ]);
+        $sku = $add->getRequest()->getParam('sku');
 
+        $hiddent = $add->getRequest()->getParam('addedfromamasty');
 
+        if ($hiddent == 1) {
+            $product = $this->productRepository->get($sku);
+            try {
+                $add->getRequest()->setParams([
+                    'product' => $product->getId(),
+                    'qty' => $add->getRequest()->getParam('qty'),
+                ]);
+            } catch (NoSuchEntityException $exception) {
+                $this->messageManager->addExceptionMessage($exception, __('Совпадений в базе не найдено!'));
+            } catch (LocalizedException $exception) {
+                $this->messageManager->addExceptionMessage($exception);
+            } catch (\Exception $exception) {
+                $this->messageManager->addExceptionMessage($exception);
+            }
+        }
     }
 }
